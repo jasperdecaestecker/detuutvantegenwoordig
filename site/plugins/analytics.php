b2cdc6a4aa5255a3416bf9b82c997e49dbeee05c
Ken Verhaegen <kenverhaegen@gmail.com>
2015-11-04 22:02:49 +0100

    Create analytics.php

---
+++ site/plugins/analytics.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Analytics plugin for Kirby
+ *
+ * @author Authorless
+ * @version 1.0
+ */
+function analytics()
+{
+    if (c::get('analytics', false) === false) {
+        return;
+    }
+
+    $id = c::get('analytics.id');
+    if ($id === null) {
+        return;
+    }
+
+    $anonymize = c::get('analytics.anonymize', false);
+    if (is_bool($anonymize) === false) {
+        $anonymize = false;
+    }
+    
+    $templateData = compact('id', 'anonymize');
+
+    return tpl::load(__DIR__ . DS . 'template.php', $templateData);
+}
